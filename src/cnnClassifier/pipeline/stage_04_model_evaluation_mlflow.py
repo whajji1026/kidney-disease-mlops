44eebfc4e5d6568e889dbb7b5590dda3d51f9b56
whajji1026 <wyeemhajjy26@gmail.com>
2024-10-14 23:35:42 +0100

    dvc added

--- src/cnnClassifier/pipeline/stage_04_model_evaluation_mlflow.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation_mlflow.py
@@ -2,10 +2,13 @@ from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.model_evaluation_mlflow import Evaluation
 from cnnClassifier import logger
 
+
+
 STAGE_NAME = "Evaluation stage"
 
+
 class EvaluationPipeline:
-    def __init__(self) :
+    def __init__(self):
         pass
 
     def main(self):
@@ -18,3 +21,15 @@ class EvaluationPipeline:
 
 
 
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+            
